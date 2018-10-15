210e3141a184f2917ab7b7b798c31639e65e8c66
Gabriel Castro <GabrielCastro@users.noreply.github.com>
2018-10-15 11:48:11 -0400

    Add more tests for getting stats

--- __tests__/queueGauges.ts
+++ __tests__/queueGauges.ts
@@ -13,6 +13,10 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
+  await testData.queue.clean(0, 'completed');
+  await testData.queue.clean(0, 'active');
+  await testData.queue.clean(0, 'delayed');
+  await testData.queue.clean(0, 'failed');
   await testData.queue.empty();
   await testData.queue.close();
 });
@@ -31,5 +35,93 @@ it('should list 1 queued job', async () => {
 
   await getStats(prefix, name, queue, guages);
 
+  expect(registry.metrics()).toMatchSnapshot();
+});
+
+it('should list 1 completed job', async () => {
+  const {
+    name,
+    queue,
+    prefix,
+    guages,
+    registry,
+  } = testData;
+
+  queue.process(async job => {
+    expect(job).toMatchObject({ data: { a: 1 } });
+  });
+  const job = await queue.add({ a: 1 });
+  await job.finished();
+
+  await getStats(prefix, name, queue, guages);
+
+  expect(registry.metrics()).toMatchSnapshot();
+});
+
+it('should list 1 failed job', async () => {
+  const {
+    name,
+    queue,
+    prefix,
+    guages,
+    registry,
+  } = testData;
+
+  queue.process(async job => {
+    expect(job).toMatchObject({ data: { a: 1 } });
+    throw new Error('expected');
+  });
+  const job = await queue.add({ a: 1 });
+
+  await expect(job.finished()).rejects.toThrow(/expected/);
+
+  await getStats(prefix, name, queue, guages);
+
+  expect(registry.metrics()).toMatchSnapshot();
+});
+
+it('should list 1 delayed job', async () => {
+  const {
+    name,
+    queue,
+    prefix,
+    guages,
+    registry,
+  } = testData;
+
+  await queue.add({ a: 1 }, { delay: 100_000 });
+
+  await getStats(prefix, name, queue, guages);
+
+  expect(registry.metrics()).toMatchSnapshot();
+});
+
+it('should list 1 active job', async () => {
+  const {
+    name,
+    queue,
+    prefix,
+    guages,
+    registry,
+  } = testData;
+
+  let jobStartedResolve!: () => void;
+  let jobDoneResolve!: () => void;
+  const jobStartedPromise = new Promise(resolve => jobStartedResolve = resolve);
+  const jobDonePromise = new Promise(resolve => jobDoneResolve = resolve);
+
+  queue.process(async () => {
+    jobStartedResolve();
+    await jobDonePromise;
+  });
+  const job = await queue.add({ a: 1 });
+
+  await jobStartedPromise;
+  await getStats(prefix, name, queue, guages);
+  expect(registry.metrics()).toMatchSnapshot();
+  jobDoneResolve();
+  await job.finished();
+
+  await getStats(prefix, name, queue, guages);
   expect(registry.metrics()).toMatchSnapshot();
 });
