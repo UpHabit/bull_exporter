616536c1c3c878f6d6c315f02a8d5d87ed07a50f
Jan Halama <halama@jablotron.cz>
2020-10-21 08:54:16 +0200

    test: add MetricCollector total failed jobs collection test

--- __tests__/metricsCollector.ts
+++ __tests__/metricsCollector.ts
@@ -1,7 +1,7 @@
 import { logger } from '../src/logger';
 import { MetricCollector } from '../src/metricCollector';
 import { makeQueue } from './create.util';
-import promClient from 'prom-client';
+import promClient, { Registry } from 'prom-client';
 
 
 describe('metricsCollector', () => {
@@ -23,6 +23,7 @@ describe('metricsCollector', () => {
   });
 
   afterEach(async () => {
+    (testData.registry as Registry).clear();
     await collector.close();
     await testData.queue.clean(0, 'completed');
     await testData.queue.clean(0, 'active');
@@ -32,7 +33,7 @@ describe('metricsCollector', () => {
     await testData.queue.close();
   });
 
-  it('should list 1 completed job', async () => {
+  it('should list 2 total completed job', async () => {
     const {
       queue,
     } = testData;
@@ -40,13 +41,46 @@ describe('metricsCollector', () => {
     queue.process(async () => {
     });
 
+    const metricCollected = new Promise((resolve) => {
+      collector.registerJobCompletionCollectedHandler(() => {
+        resolve();
+      });
+    });
+
     const job1 = (await queue.add({ a: 1 }));
     const job2 = (await queue.add({ a: 2 }));
     await job1.finished();
     await job2.finished();
 
+    await metricCollected;
+
     const metrics = promClient.register.metrics();
 
     expect(metrics).toMatch(/^test_stat_total_completed{prefix="test-queue",queue="TestQueue"} 2$/m);
+  });
+
+  it('should list 1 total failed job', async () => {
+    const {
+      queue,
+    } = testData;
+
+    queue.process(async () => {
+      throw new Error('expected');
+    });
+
+    const metricCollected = new Promise((resolve) => {
+      collector.registerJobFailureCollectedHandler(() => {
+        resolve();
+      });
+    });
+
+    const job = (await queue.add({ a: 1 }));
+    await expect(job.finished()).rejects.toThrow(/expected/);
+
+    await metricCollected;
+
+    const metrics = promClient.register.metrics();
+
+    expect(metrics).toMatch(/^test_stat_total_failed{prefix="test-queue",queue="TestQueue"} 1$/m);
   });
 });
