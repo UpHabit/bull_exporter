16e26e5f60d23c6b73f0847eef2045606fb36166
Marius <marius@juro.com>
2023-02-07 15:30:41 +0200

    fix: transpile errors

--- src/options.ts
+++ src/options.ts
@@ -14,45 +14,50 @@ export interface Options {
 }
 
 export function getOptionsFromArgs(...args: string[]): Options {
-  return yargs
-    .version(version)
-    .alias('V', 'version')
-    .options({
-      url: {
-        alias: 'u',
-        describe: 'A redis connection url',
-        default: 'redis://127.0.0.1:6379',
-        demandOption: true,
-      },
-      prefix: {
-        alias: 'p',
-        default: 'bull',
-        demandOption: true,
-      },
-      metricPrefix: {
-        alias: 'm',
-        default: 'bull_queue_',
-        defaultDescription: 'prefix for all exported metrics',
-        demandOption: true,
-      },
-      once: {
-        alias: 'n',
-        default: false,
-        type: 'boolean',
-        description: 'Print stats and exit without starting a server',
-      },
-      port: {
-        default: 9538,
-      },
-      autoDiscover: {
-        default: false,
-        alias: 'a',
-        type: 'boolean',
-      },
-      bindAddress: {
-        alias: 'b',
-        description: 'Address to listen on',
-        default: '0.0.0.0',
-      },
-    }).parse(args);
+  const options = yargs
+		.version(version)
+		.alias('V', 'version')
+		.options({
+			url: {
+				alias: 'u',
+				describe: 'A redis connection url',
+				default: 'redis://127.0.0.1:6379',
+				demandOption: true,
+			},
+			prefix: {
+				alias: 'p',
+				default: 'bull',
+				demandOption: true,
+			},
+			metricPrefix: {
+				alias: 'm',
+				default: 'bull_queue_',
+				defaultDescription: 'prefix for all exported metrics',
+				demandOption: true,
+			},
+			once: {
+				alias: 'n',
+				default: false,
+				type: 'boolean',
+				description: 'Print stats and exit without starting a server',
+			},
+			port: {
+				default: 9538,
+			},
+			autoDiscover: {
+				default: false,
+				alias: 'a',
+				type: 'boolean',
+			},
+			bindAddress: {
+				alias: 'b',
+				description: 'Address to listen on',
+				default: '0.0.0.0',
+			},
+		})
+		.option('string-option', {
+			string: true,
+		})
+		.parse(args);
+	return { ...options, _: options._ as string[] };
 }
