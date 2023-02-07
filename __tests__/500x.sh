18ec02d227d444ee26479a012666b6ce868b4618
Viktors Buls <viktors@juro.com>
2023-02-07 18:38:57 +0200

    test: use sh seq loop instead of zx

--- __tests__/500x.sh
+++ __tests__/500x.sh
@@ -1,1 +1,3 @@
-npx zx -e 'for (let i = 1; i <= 500; i += 1) { console.log(`==== ${i} ====`); const child = await $`npm run test`; if (child.exitCode !== 0) throw new Error('fail') }'
+# npx zx -e 'for (let i = 1; i <= 500; i += 1) { console.log(`==== ${i} ====`); const child = await $`npm run test`; if (child.exitCode !== 0) throw new Error('fail') }'
+
+for i in $(seq 1 500); do echo "==== $i ===="; npm run test || break; done
